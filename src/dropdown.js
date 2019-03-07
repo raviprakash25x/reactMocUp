3286a5cb60434c6dfce2dafe17278509b9a55106
Ravi Prakash <ravi.rp.prakash@oracle.com>
2019-03-07 16:16:36 +0530

    new files

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,3 +1,17 @@
 /*
 Dropdown combo-box component to be implemented by Kalpit
 */
+
+
+import React, { Component } from 'react';
+
+class Dropdown extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default Dropdown;
--- src/popUp.js
+++ src/popUp.js
@@ -1,3 +1,16 @@
 /*
 PopUp window component to be implemted by Madhu
 */
+
+import React, { Component } from 'react';
+
+class Popup extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default Popup;
