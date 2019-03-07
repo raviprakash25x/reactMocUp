531c74f232c7d3d0825000782497c0011ae5b120
Ravi Prakash <ravi.rp.prakash@oracle.com>
2019-03-07 16:49:05 +0530

    test

--- src/dropdown.js
+++ src/dropdown.js
@@ -3,15 +3,3 @@ Dropdown combo-box component to be implemented by Kalpit
 */
 
 
-import React, { Component } from 'react';
-
-class Dropdown extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
-  }
-}
-
-export default Dropdown;
