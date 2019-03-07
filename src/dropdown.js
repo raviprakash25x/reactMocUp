b050f5845aada6b739de3726cd6688b102c65ade
Kalpit Shah <kalpit.shah@oracle.com>
2019-03-07 16:44:06 +0530

    Commit

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,3 +1,22 @@
-/*
-Dropdown combo-box component to be implemented by Kalpit
-*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Dropdown extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+    }
+    render()
+    {
+        const optionList=<div className="dropdown"><select value={this.props.optionValue} onChange={this.props.handleChange}>
+		                    <option value="7">Last 7 days</option>
+		                    <option value="30">Last 30 days</option>
+		                    <option value="90">Last 90 days</option>
+                        </select></div>;
+        return optionList;
+    }
+}
+
+export default Dropdown;
